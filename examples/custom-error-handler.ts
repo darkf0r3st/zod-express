6edc7c981bc2a579db0240d6fe47029567784758
Shahar Kedar <shahar@riseup.co.il>
2024-03-14 20:58:51 +0200

    Add an example of registering a custom error handling function

---
+++ examples/custom-error-handler.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import z from 'zod';
+import { parsingMiddleWare } from '../src/zodExpressLite';
+
+const app = express();
+app.use(express.json());
+
+const DummySchema = z.object({});
+type Dummy = z.infer<typeof DummySchema>;
+
+async function throwAnError(dummy: Dummy): Promise<string> {
+  console.log(dummy);
+  throw new Error('This is an unexpected error');
+}
+
+/**
+ * By default, the parsingMiddleWare function will send a 500 status code
+ * on an unexpected error. You can override this behavior by providing an
+ * unexpectedErrorHandler function.
+ */
+app.post('/get-user-name', parsingMiddleWare(throwAnError, DummySchema,
+  (error, req, res) => {
+    res.status(500).send({ message: error.message });
+  }));
+
+
+
