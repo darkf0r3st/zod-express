a79de52982f7ce6c513a9351d171e962db35dab9
Shahar Kedar <shahar@riseup.co.il>
2024-03-20 21:21:32 +0200

    Increase test coverage to 100%

---
+++ tests/unit-test.spec.ts
@@ -0,0 +1,11 @@
+import { parsingMiddleware } from '../src';
+import { describe, expect, it } from 'vitest';
+
+describe('Passing an invalid parser', () => {
+  it('Should throw an error', () => {
+    expect(() => {
+      parsingMiddleware(async () => { }, {} as never);
+    }).toThrowError('Parser can either by a CustomParsingFunction or a ZodType');
+  });
+});
+
